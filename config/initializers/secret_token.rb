ea54b90ad36d640234070193a4c3d2378bd3daa9
Nick Coelius <ncoelius@gmail.com>
2011-07-19 19:28:51 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StumbleAbout::Application.config.secret_token = '6c510a4d3dbe8e2c4e27140e7922d2654ffbe8517332c9422b65e32e3e77eb8efcb00414b4d260a12cd41af3249f77f1094ee839da643cc709a366210c0852d2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+StumbleAbout::Application.config.session_store :cookie_store, :key => '_stumble_about_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# StumbleAbout::Application.config.session_store :active_record_store
